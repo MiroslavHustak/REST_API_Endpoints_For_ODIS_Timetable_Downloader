a4d7af48d30f0c93f39b2e9ecb0b1a51a1e05986
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2025-11-18 22:34:51 +0100

    18-11-2025

--- REST_API_Endpoints_For_ODIS_Timetable_Downloader/MyFsToolkit/CEBuilders.fs
+++ REST_API_Endpoints_For_ODIS_Timetable_Downloader/MyFsToolkit/CEBuilders.fs
@@ -3,37 +3,56 @@
 module Builders =
 
     [<Struct>]
-    type internal MyBuilder4 = MyBuilder4 with       
-        member _.Bind(resultExpr, nextFunc) = 
-            match fst resultExpr with
-            | Ok value  -> nextFunc value 
-            | Error err -> async { return () }, (snd resultExpr) err
-        member _.Return x = x  
+    type internal MyBuilder4 = MyBuilder4 with  
+        member _.Recover(m, nextFunc) = //neni monada, nesplnuje vsechny 3 monadicke zakony (left/right identity, associativity)   
+            match m with
+            | (Ok v, _)           
+                -> nextFunc v 
+            | (Error err, handler) 
+                -> async { return () }, handler err
+
+        member inline this.Bind(m, f) = this.Recover(m, f) //an alias to prevent confusion
         
+        member _.Zero () = ()       
+        member _.Return x = x
+        member _.ReturnFrom x = x     
+           
     let internal pyramidOfAsyncInferno = MyBuilder4 
 
     //**************************************************************************************
-           
+
     [<Struct>]
-    type internal MyBuilder3 = MyBuilder3 with       
-         member _.Bind(resultExpr, nextFunc) = 
-             match fst resultExpr with
-             | Ok value  -> nextFunc value 
-             | Error err -> (snd resultExpr) err
-         member _.Return x = x  
+    type internal MyBuilder3 = MyBuilder3 with   
+        member _.Recover(m, nextFunc) = //neni monada, nesplnuje vsechny 3 monadicke zakony   
+            match m with
+            | (Ok v, _)           
+                -> nextFunc v 
+            | (Error err, handler) 
+                -> handler err
+
+        member inline this.Bind(m, f) = this.Recover(m, f) //an alias to prevent confusion
+        
+        member _.Zero () = ()       
+        member _.Return x = x
+        member _.ReturnFrom x = x     
+        
+    let internal pyramidOfInferno = MyBuilder3             
      
-    let internal pyramidOfInferno = MyBuilder3 
-    
     //**************************************************************************************
 
     [<Struct>]
     type internal MyBuilder = MyBuilder with    
-         member _.Bind(condition, nextFunc) =
-             match fst condition with
-             | false -> snd condition
-             | true  -> nextFunc()  
-         member _.Return x = x
-         member _.Using x = x
+        member _.Bind(m : bool * (unit -> 'a), nextFunc : unit -> 'a) : 'a =
+              match m with
+              | (false, handleFalse)
+                  -> handleFalse()
+              | (true, _)
+                  -> nextFunc()    
+          member _.Return x : 'a = x   
+          member _.ReturnFrom x : 'a = x 
+          member _.Using(x : 'a, _body: 'a -> 'b) : 'b = _body x    
+          member _.Delay(f : unit -> 'a) = f()
+          member _.Zero() = ()   
 
     let internal pyramidOfHell = MyBuilder
 
@@ -41,12 +60,12 @@ module Builders =
 
     [<Struct>]
     type internal Builder2 = Builder2 with    
-        member _.Bind((optionExpr, err), nextFunc) =
-            match optionExpr with
-            | Some value -> nextFunc value 
-            | _          -> err  
+        member _.Bind((m, recovery), nextFunc) =
+            match m with
+            | Some v -> nextFunc v
+            | None   -> recovery    
         member _.Return x : 'a = x   
-        member _.ReturnFrom x : 'a = x 
+        member _.ReturnFrom x : 'a = x
         member _.TryFinally(body, compensation) =
             try body()
             finally compensation()
